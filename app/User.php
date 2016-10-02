57c58a27d2feb9e5c6de0274cf0b3a2118601fe3
Piethein Strengholt <piethein@strengholt-online.nl>
2016-10-02 13:13:10 +0200

    replace user Model with NeoEloquent

--- app/User.php
+++ app/User.php
@@ -2,10 +2,24 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Vinelab\NeoEloquent\Eloquent\Relations\MorphMany;
+use Vinelab\NeoEloquent\Eloquent\Model as NeoEloquent;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+class User extends NeoEloquent implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
+    use Authenticatable, Authorizable, CanResetPassword;
+
+    protected $label = 'User';
+
     /**
      * The attributes that are mass assignable.
      *
